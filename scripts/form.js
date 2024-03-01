3e69c014f6c53293ad23cdb8f1ab6b51b74a3784
emh68 <elihansen@yahoo.com>
2024-02-29 19:55:52 -0700

    edits

--- scripts/form.js
+++ scripts/form.js
@@ -11,16 +11,34 @@ function displayRatingValue() {
 
 let password = document.getElementById('pwd');
 let confirmPassword = document.getElementById('confirmPwd');
-const messageDiv = document.querySelector(".message");
+let emailInput = document.getElementById('email');
+const passwordMessage = document.querySelector(".password-message");
+const emailMessage = document.querySelector(".email-message");
+const form = document.querySelector('form');
 
 confirmPassword.addEventListener('focusout', function () {
     if (password.value != confirmPassword.value) {
-        messageDiv.textContent = "Your passwords do not match";
-        confirmPassword.value = "";
-        confirmPassword.focus();
-
+        passwordMessage.textContent = "Your passwords do not match";
     } else {
-        messageDiv.textContent = "";
-
+        passwordMessage.textContent = "";
     }
 });
+
+emailInput.addEventListener('focusout', function () {
+    let emailValue = this.value;
+    if (!isEmailValid(emailValue)) {
+        emailMessage.textContent = "Please enter a valid byui.edu email address.";
+    } else {
+        emailMessage.textContent = "";
+    }
+});
+
+form.addEventListener('submit', function (event) {
+    if (!isEmailValid || !isPasswordValid(emailValue, PasswordValue)) {
+        event.preventDefault();
+    }
+});
+
+function isEmailValid(email) {
+    return /@byui\.edu$/.test(email);
+}
