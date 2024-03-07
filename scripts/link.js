ab5b593baf8e5da4a2cdf80b18df54ee9bef0646
emh68 <elihansen@yahoo.com>
2024-03-06 17:01:05 -0700

    edits

--- scripts/link.js
+++ scripts/link.js
@@ -1,5 +1,5 @@
-const baseURL = "https://www.github.io/emh68/wdd230/";
-const linksURL = "https://www.github.io/emh68/wdd230/data/links.json";
+const baseURL = "https://emh68.github.io/wdd230/";
+const linksURL = "https://emh68.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
