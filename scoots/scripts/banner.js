507e456c4401b4c85027ca3a703d7c0f242d4321
emh68 <elihansen@yahoo.com>
2024-03-31 19:05:29 -0600

    edits

---
+++ scoots/scripts/banner.js
@@ -0,0 +1,19 @@
+// Homepage banner
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current day of the week (0 = Sun, 1 = Mon, etc.)
+    // const currentDayOfWeek = new Date().getDay();
+    const banner = document.querySelector(".banner");
+
+    // Check if it is Monday, Tuesday or Wednesday
+    // if (currentDayOfWeek >= 1 && currentDayOfWeek <= 3) {
+    //     banner.style.display = "block"; // Show the banner
+    // } else {
+    //     banner.style.display = "none"; // Hide the banner
+    // }
+
+    // Close banner when closeButton clicked
+    const closeButton = document.querySelector(".banner-close");
+    closeButton.addEventListener("click", function () {
+        banner.style.display = "none";
+    });
+});
--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -1,7 +1,7 @@
 // Weather API and calculate wind chill
 document.addEventListener('DOMContentLoaded', function () {
-    const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=40.761&lon=-111.891&appid=b2384255491935468122e0e1ee279982&units=imperial';
-    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=40.761&lon=-111.891&appid=b2384255491935468122e0e1ee279982&units=imperial';
+    const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=20.5066&lon=-86.944&appid=b2384255491935468122e0e1ee279982&units=imperial';
+    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.5066&lon=-86.944&appid=b2384255491935468122e0e1ee279982&units=imperial';
 
     async function apiFetch() {
         try {
@@ -11,12 +11,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 const forecastData = await forecastResponse.json();
 
                 displayResults(currentData, forecastData);
-                const currentWindChillElement = document.querySelectorAll('#current-wind-chill');
-                const tomorrowWindChillElement = document.querySelectorAll('#tomorrow-wind-chill');
-                const dayAfterTomorrowWindChillElement = document.querySelectorAll('#day-after-tomorrow-wind-chill');
-                calculateWindChill(currentData.main.temp, currentData.wind.speed, currentWindChillElement);
-                calculateWindChill(forecastData.list[2].main.temp, forecastData.list[2].wind.speed, tomorrowWindChillElement);
-                calculateWindChill(forecastData.list[10].main.temp, forecastData.list[10].wind.speed, dayAfterTomorrowWindChillElement);
 
             } else {
                 throw Error(await response.text());
@@ -46,52 +40,30 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#current-temp-low').innerHTML = `${Math.round(currentData.main.temp_min)}&deg;F`;
         document.querySelector('#current-temp-high').innerHTML = `${Math.round(currentData.main.temp_max)}&deg;F`;
         document.querySelector('#current-wind-speed').innerHTML = `${Math.round(currentData.wind.speed)} mph`;
+        document.querySelector('#current-humidity').innerHTML = `${Math.round(currentData.main.humidity)}%`;
+        document.querySelector('.banner-temperature').innerHTML = `${Math.round(currentData.main.temp_max)}&deg;F`;
 
         // Display tomorrow's weather
-        const tomorrowMonthDay = formatUnixTimestamp(forecastData.list[2].dt);
+        const tomorrowMonthDay = formatUnixTimestamp(forecastData.list[5].dt);
         document.querySelector('#tomorrow-month-day').innerHTML = tomorrowMonthDay;
-        document.querySelector('#tomorrow-temp').innerHTML = `${Math.round(forecastData.list[2].main.temp)}&deg;F`;
-        document.querySelector('#tomorrow-weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${forecastData.list[1].weather[0].icon}.png`);
+        document.querySelector('#tomorrow-temp').innerHTML = `${Math.round(forecastData.list[5].main.temp)}&deg;F`;
+        document.querySelector('#tomorrow-weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${forecastData.list[5].weather[0].icon}.png`);
         document.querySelector('#tomorrow-weather-icon').setAttribute('alt', 'Weather icon');
-        document.querySelector('#tomorrow-desc').textContent = `${forecastData.list[2].weather[0].description}`;
-        document.querySelector('#tomorrow-temp-low').innerHTML = `${Math.round(forecastData.list[2].main.temp_min)}&deg;F`;
-        document.querySelector('#tomorrow-temp-high').innerHTML = `${Math.round(forecastData.list[2].main.temp_max)}&deg;F`;
-        document.querySelector('#tomorrow-wind-speed').innerHTML = `${Math.round(forecastData.list[2].wind.speed)} mph`;
-
+        document.querySelector('#tomorrow-desc').textContent = `${forecastData.list[5].weather[0].description}`;
+        document.querySelector('#tomorrow-temp-low').innerHTML = `${Math.round(forecastData.list[5].main.temp_min)}&deg;F`;
+        document.querySelector('#tomorrow-temp-high').innerHTML = `${Math.round(forecastData.list[5].main.temp_max)}&deg;F`;
+        document.querySelector('#tomorrow-wind-speed').innerHTML = `${Math.round(forecastData.list[5].wind.speed)} mph`;
+        document.querySelector('#tomorrow-humidity').innerHTML = `${Math.round(forecastData.list[5].main.humidity)}%`;
         // Display day after tomorrow's weather
-        const dayAfterTomorrowMonthDay = formatUnixTimestamp(forecastData.list[10].dt);
+        const dayAfterTomorrowMonthDay = formatUnixTimestamp(forecastData.list[13].dt);
         document.querySelector('#day-after-tomorrow-month-day').innerHTML = dayAfterTomorrowMonthDay;
-        document.querySelector('#day-after-tomorrow-temp').innerHTML = `${Math.round(forecastData.list[10].main.temp)}&deg;F`;
-        document.querySelector('#day-after-tomorrow-weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${forecastData.list[9].weather[0].icon}.png`);
+        document.querySelector('#day-after-tomorrow-temp').innerHTML = `${Math.round(forecastData.list[13].main.temp)}&deg;F`;
+        document.querySelector('#day-after-tomorrow-weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${forecastData.list[13].weather[0].icon}.png`);
         document.querySelector('#day-after-tomorrow-weather-icon').setAttribute('alt', 'Weather icon');
-        document.querySelector('#day-after-tomorrow-desc').textContent = `${forecastData.list[10].weather[0].description}`;
-        document.querySelector('#day-after-tomorrow-temp-low').innerHTML = `${Math.round(forecastData.list[10].main.temp_min)}&deg;F`;
-        document.querySelector('#day-after-tomorrow-temp-high').innerHTML = `${Math.round(forecastData.list[10].main.temp_max)}&deg;F`;
-        document.querySelector('#day-after-tomorrow-wind-speed').innerHTML = `${Math.round(forecastData.list[10].wind.speed)} mph`;
+        document.querySelector('#day-after-tomorrow-desc').textContent = `${forecastData.list[13].weather[0].description}`;
+        document.querySelector('#day-after-tomorrow-temp-low').innerHTML = `${Math.round(forecastData.list[13].main.temp_min)}&deg;F`;
+        document.querySelector('#day-after-tomorrow-temp-high').innerHTML = `${Math.round(forecastData.list[13].main.temp_max)}&deg;F`;
+        document.querySelector('#day-after-tomorrow-wind-speed').innerHTML = `${Math.round(forecastData.list[13].wind.speed)} mph`;
+        document.querySelector('#day-after-tomorrow-humidity').innerHTML = `${Math.round(forecastData.list[13].main.humidity)}%`;
     }
-
-    function calculateWindChill(temperature, windSpeed, windChillElements) {
-        // Check if the input values meet the requirements
-        if (temperature <= 50 && windSpeed > 3.0) {
-            // Calculate wind chill factor using the formula
-            let windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-
-            // Round the result to a whole number
-            windChill = Math.round(windChill);
-
-            // Update wind chill for each element
-            windChillElements.forEach(element => {
-                element.innerHTML = `Wind chill: ${windChill}&deg;F`;
-                element.style.display = 'inline';
-            });
-
-        } else {
-            // If input values do not meet the requirements, hide wind chill for each element
-            windChillElements.forEach(element => {
-                element.innerHTML = 'Wind chill: None';
-                element.style.display = 'inline';
-            });
-        }
-    }
-
 });
