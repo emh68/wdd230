e709d51abc723c0e8740b049a8410ea373bc7649
emh68 <elihansen@yahoo.com>
2024-04-03 17:19:11 -0600

    edits

--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -37,8 +37,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#current-weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${currentData.weather[0].icon}.png`);
         document.querySelector('#current-weather-icon').setAttribute('alt', 'Weather icon');
         document.querySelector('#current-desc').textContent = `${currentData.weather[0].description}`;
-        document.querySelector('#current-temp-low').innerHTML = `${Math.round(currentData.main.temp_min)}&deg;F`;
-        document.querySelector('#current-temp-high').innerHTML = `${Math.round(currentData.main.temp_max)}&deg;F`;
+        document.querySelector('#current-temp-low').innerHTML = `${Math.floor(currentData.main.temp_min)}&deg;F`;
+        document.querySelector('#current-temp-high').innerHTML = `${Math.ceil(currentData.main.temp_max)}&deg;F`;
         document.querySelector('#current-wind-speed').innerHTML = `${Math.round(currentData.wind.speed)} mph`;
         document.querySelector('#current-humidity').innerHTML = `${Math.round(currentData.main.humidity)}%`;
         document.querySelector('.banner-temperature').innerHTML = `${Math.round(currentData.main.temp_max)}&deg;F`;
@@ -50,8 +50,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#tomorrow-weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${forecastData.list[5].weather[0].icon}.png`);
         document.querySelector('#tomorrow-weather-icon').setAttribute('alt', 'Weather icon');
         document.querySelector('#tomorrow-desc').textContent = `${forecastData.list[5].weather[0].description}`;
-        document.querySelector('#tomorrow-temp-low').innerHTML = `${Math.round(forecastData.list[5].main.temp_min)}&deg;F`;
-        document.querySelector('#tomorrow-temp-high').innerHTML = `${Math.round(forecastData.list[5].main.temp_max)}&deg;F`;
+        document.querySelector('#tomorrow-temp-low').innerHTML = `${Math.floor(forecastData.list[5].main.temp_min)}&deg;F`;
+        document.querySelector('#tomorrow-temp-high').innerHTML = `${Math.ceil(forecastData.list[5].main.temp_max)}&deg;F`;
         document.querySelector('#tomorrow-wind-speed').innerHTML = `${Math.round(forecastData.list[5].wind.speed)} mph`;
         document.querySelector('#tomorrow-humidity').innerHTML = `${Math.round(forecastData.list[5].main.humidity)}%`;
         // Display day after tomorrow's weather
@@ -61,8 +61,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#day-after-tomorrow-weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${forecastData.list[13].weather[0].icon}.png`);
         document.querySelector('#day-after-tomorrow-weather-icon').setAttribute('alt', 'Weather icon');
         document.querySelector('#day-after-tomorrow-desc').textContent = `${forecastData.list[13].weather[0].description}`;
-        document.querySelector('#day-after-tomorrow-temp-low').innerHTML = `${Math.round(forecastData.list[13].main.temp_min)}&deg;F`;
-        document.querySelector('#day-after-tomorrow-temp-high').innerHTML = `${Math.round(forecastData.list[13].main.temp_max)}&deg;F`;
+        document.querySelector('#day-after-tomorrow-temp-low').innerHTML = `${Math.floor(forecastData.list[13].main.temp_min)}&deg;F`;
+        document.querySelector('#day-after-tomorrow-temp-high').innerHTML = `${Math.ceil(forecastData.list[13].main.temp_max)}&deg;F`;
         document.querySelector('#day-after-tomorrow-wind-speed').innerHTML = `${Math.round(forecastData.list[13].wind.speed)} mph`;
         document.querySelector('#day-after-tomorrow-humidity').innerHTML = `${Math.round(forecastData.list[13].main.humidity)}%`;
     }
